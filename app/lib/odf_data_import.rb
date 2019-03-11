57a27b1108f190be720847fb29cc37819c9fe2fb
Aaron Sumner <asumner@mac.com>
2019-03-10 20:26:02 -0700

    Parse individual item from ODF JSON
    We'll want to access individual objects provided by the ODF JSON data
    feed. For now, just ensure that we can access all the key/value pairs
    provided per object.

--- app/lib/odf_data_import.rb
+++ app/lib/odf_data_import.rb
@@ -1,5 +1,9 @@
 class OdfDataImport
-  def import
-    JSON.parse(File.read(Rails.root.join("data.json")))
+  def initialize
+    @data = JSON.parse(File.read(Rails.root.join("data.json")))
+  end
+
+  def data
+    @data
   end
 end
--- spec/lib/odf_data_import_spec.rb
+++ spec/lib/odf_data_import_spec.rb
@@ -2,7 +2,30 @@ require "spec_helper"
 
 describe "ODF data import" do
   it "finds 63 records" do
-    data = OdfDataImport.new
-    expect(data.import.length).to eq 63
+    data = OdfDataImport.new.data
+    expect(data.length).to eq 63
+  end
+
+  it "parses an individual record" do
+    data = OdfDataImport.new.data
+    expect(data.first).to eq(
+      {
+        ":@computed_region_i25a_xk5b" => "2412",
+        "amentities" => "restrooms; picnic tables",
+        "contact_number" => "503-815-6800",
+        "description" => "Visitor Center",
+        "district" => "Forest Grove",
+        "location" => {
+          "type" => "Point",
+          "coordinates" => [-123.560477, 45.586471]
+        },
+        "name" => "Tillamook Forest Center",
+        "number_of_sites" => "N/A",
+        "overnight_fee" => "N/A",
+        "season" => "March - November",
+        "state_forest" => "Tillamook",
+        "type" => "Hike, Day Use"
+      }
+    )
   end
 end
