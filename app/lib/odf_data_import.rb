d4ad8aad2c56d1d3e22ac65d09b6f77ad6f977cd
Aaron Sumner <asumner@mac.com>
2019-03-13 22:00:39 -0700

    Import opportunities from ODF JSON
    Opportunities are the actual recreational opportunities offered by ODF
    according to the JSON feed they provide. Using the JSON, add an importer
    to populate the opportunities table:
    
    * Do not yet allow updating an existing opportunity; just fail with a
      validation for now (wait for a new feed to rethink this)
    * Punt on importing types for now, that will be a many-to-many and
      require a little extra work (coming next-ish)
    * Continue using the whole JSON feed as a fixture for the tests. It's
      small enough and provides a sanity check during development to just
      have the whole thing under test

--- app/lib/odf_data_import.rb
+++ app/lib/odf_data_import.rb
@@ -3,8 +3,32 @@ class OdfDataImport
     @data = JSON.parse(File.read(Rails.root.join("data.json")))
   end
 
+  def import
+    @data.each do |opportunity|
+      import_opportunity(opportunity)
+    end
+  end
+
   def data
     @data
+  end
+
+  def import_opportunity(attributes)
+    Opportunity.create do |opp|
+      opp.name = attributes["name"]
+      opp.computed_region = attributes[':@computed_region_i25a_xk5b'].to_i
+      opp.amenities = attributes["amentities"] # sic
+      opp.contact_number = attributes["contact_number"]
+      opp.description = attributes["description"]
+      opp.district = District.find_or_create_by(name: attributes["district"])
+      opp.latitude = attributes["location"]["coordinates"][0]
+      opp.longitude = attributes["location"]["coordinates"][1]
+      opp.number_of_sites = attributes["number_of_sites"]
+      opp.overnight_fee = attributes["overnight_fee"]
+      opp.season = attributes["season"]
+      opp.state_forest = \
+        StateForest.find_or_create_by(name: attributes["state_forest"])
+    end
   end
 
   def state_forests
--- spec/lib/odf_data_import_spec.rb
+++ spec/lib/odf_data_import_spec.rb
@@ -29,6 +29,30 @@ describe "ODF data import" do
     )
   end
 
+  it "imports opportunities" do
+    OdfDataImport.new.import
+
+    forest_grove_district = District.find_by(name: "Forest Grove")
+    tillamook_state_forest = StateForest.find_by(name: "Tillamook")
+
+    expect(Opportunity.count).to eq 63
+    expect(Opportunity.first).to have_attributes(
+      computed_region: 2412,
+      amenities: "restrooms; picnic tables",
+      contact_number: "503-815-6800",
+      description: "Visitor Center",
+      district: forest_grove_district,
+      latitude: -123.560477,
+      longitude: 45.586471,
+      name: "Tillamook Forest Center",
+      number_of_sites: "N/A",
+      overnight_fee: "N/A",
+      season: "March - November",
+      state_forest: tillamook_state_forest,
+      # type: "Hike, Day Use",
+    )
+  end
+
   it "gathers a list of state forests" do
     data = OdfDataImport.new
     expect(data.state_forests).to eq [
