85bcc4c7b3566121b990a2e237e22f57092763bf
Aaron Sumner <asumner@mac.com>
2019-03-13 19:40:00 -0700

    Add type model to store opportunity types
    The JSON output of ODF recreational opportunities contains a "type"
    string for each record (opportunity). The string is a comma-separated
    list of stuff to do at that location.
    
    I eventually want to store each value within each comma-separated list
    as its own type, and do a many-to-many relationship between
    opportunities and types. So this is probably getting ahead of things,
    but go ahead and create a basic Type model for this eventual
    information.

---
+++ app/models/type.rb
@@ -0,0 +1,5 @@
+class Type < ApplicationRecord
+  validates :name,
+    presence: true,
+    uniqueness: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_11_044422) do
+ActiveRecord::Schema.define(version: 2019_03_14_023713) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 2019_03_11_044422) do
   end
 
   create_table "state_forests", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "types", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
